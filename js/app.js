24e7ce6ab40412a2a43726fdbd3270691d1d8b91
Vedant <mainvedant.vns123@gmail.com>
2018-09-16 00:24:49 +0530

    Shazam

--- js/app.js
+++ js/app.js
@@ -26,7 +26,81 @@ myApp.config(function ($routeProvider) {
       templateUrl: 'pages/ama.html',
       controller: 'homeController'
   })
+  .when('/competition', {
+    templateUrl: 'pages/comp.html',
+    controller: 'compController',
+    controllerAs:'ctrl'
+})
 });
+
+myApp.controller('compController', ['$scope', '$http', '$location','$window','$rootScope', function($scope,$http,$location,$window,$rootScope){
+  $rootScope.$on('$includeContentLoaded', function() {
+    if(location.hash=="#/competition"){
+      $('html, body').animate({
+        scrollTop: $("#services").offset().top
+    }, 500);
+    
+    }
+  });
+  
+ 
+  var vm  = this;
+  $scope.reg_ind = function(form){
+
+    if(!form.$valid || vm.contact.length != 10){
+      swal({
+        title: "Error",
+        text: "The details you have entered are incorrect",
+        icon: "error",
+        button: "Ok, let me correct",
+      });
+    }else{
+      $http({
+        url: 'https://codestacks.herokuapp.com/individual',
+        method: "POST",
+        data: { fullname: form.fullname.$viewValue, email: form.email.$viewValue,address:form.address.$viewValue,contact:form.contact.$viewValue,org:form.org.$viewValue,txn:form.txn.$viewValue}
+       
+    })
+    .then(function(response) {
+      console.log(response);
+      swal({
+        title: response.data.type,
+        text: response.data.message,
+        icon: response.data.type,
+        button: "Ok",
+      });
+    });
+    }
+  }
+  $scope.reg_team = function(form){
+    if(!form.$valid || vm.contact.length != 10){
+      swal({
+        title: "Error",
+        text: "The details you have entered are incorrect",
+        icon: "error",
+        button: "Ok, let me correct",
+      });
+    }else{
+      $http({
+        url: 'https://codestacks.herokuapp.com/team',
+        method: "POST",
+        data: { team: form.teamname.$viewValue, fullname1: form.fullname1.$viewValue,email1:form.email1.$viewValue, fullname2: form.fullname2.$viewValue,email2:form.email2.$viewValue, fullname3: form.fullname3.$viewValue,email3:form.email3.$viewValue, fullname4: form.fullname4.$viewValue,email4:form.email4.$viewValue, fullname5: form.fullname5.$viewValue,email5:form.email5.$viewValue,contact:form.contact.$viewValue,org:form.org.$viewValue,txn:form.txn.$viewValue}
+       
+    })
+    .then(function(response) {
+      console.log(response);
+      swal({
+        title: response.data.type,
+        text: response.data.message,
+        icon: response.data.type,
+        button: "Ok",
+      });
+    });
+    }
+  }
+
+  
+}]);
 
 myApp.controller('homeController', ['$scope', '$http', '$location','$window','$rootScope', function($scope,$http,$location,$window,$rootScope){
 
